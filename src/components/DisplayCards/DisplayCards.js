e77699bb8e7648d86076049d2cd1fbb4eb1f8d9c
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-03-25 23:38:32 +0600

    header component added

---
+++ src/components/DisplayCards/DisplayCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayCards = () => {
+    return (
+        <div>
+            <h2>Cards</h2>
+        </div>
+    );
+};
+
+export default DisplayCards;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <p className='header-title'>Shop Watch</p>
+            <p className='header-small-title'>Choose 4 Watches</p>
+        </div>
+    );
+};
+
+export default Header;
