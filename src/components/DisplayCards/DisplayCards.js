ee889cb6f67ba90b5393fcf608c21fbb5ef0d2f5
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-03-25 23:55:15 +0600

    api data fixed and loading watch data

--- src/components/DisplayCards/DisplayCards.js
+++ src/components/DisplayCards/DisplayCards.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DisplayCards = () => {
+
+    const [watches, setWatches] = useState([]);
+    useEffect(() => {
+        fetch('watches.json')
+            .then(res => res.json())
+            .then(data => setWatches(data));
+    }, []);
+
+
     return (
         <div>
             <h2>Cards</h2>
+            <div>
+                {
+                    watches.map(watch => (
+                        <p>{watch.name}</p>
+                    ))
+                }
+            </div>
         </div>
     );
 };
